e79f01c2152c89d6d9a542cfbd1ae7be142d9190
Martín Rodríguez <slackmart@gmail.com>
2019-02-23 20:33:39 -0600

    feat: adding disqus integration

--- src/components/ArticleDetailsOverview/index.jsx
+++ src/components/ArticleDetailsOverview/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { DiscussionEmbed } from 'disqus-react'
 
 import HeaderDetail from './HeaderDetail'
 import styles from './styles.module.scss'
@@ -14,6 +15,12 @@ class ArticleDetailsOverview extends React.PureComponent {
 
   render() {
     const { title, date, author, html } = this.props
+    const disqusShortname = 'codea-com-mx'
+    const disqusConfig = {
+      identifier: 103938,
+      title: title,
+    }
+
     return (
       <div className={styles.root}>
         <HeaderDetail title={title} date={date} author={author} />
@@ -21,6 +28,8 @@ class ArticleDetailsOverview extends React.PureComponent {
           className="codea-article-details-markdown"
           dangerouslySetInnerHTML={html}
         />
+        <br />
+        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
       </div>
     )
   }
