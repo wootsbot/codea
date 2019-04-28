62f7dd0345553cb46cea8ab1ae61596dbac78b51
Jorge Luis Calleja <jorgelca1205@gmail.com>
2019-04-27 23:45:30 -0500

    [Theme]: Delete property typography.fontFamily

--- src/utils/muiTheme.js
+++ src/utils/muiTheme.js
@@ -12,18 +12,6 @@ export const muiTheme = {
     },
   },
   typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
     useNextVariants: true,
     htmlFontSize: 10,
   },
